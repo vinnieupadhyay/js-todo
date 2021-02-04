a1f0df97fd4b18fc0f9505a28cee54bf2f046f7c
Vineet Upadhyay <vinnieupadhyay@gmail.com>
2021-02-04 23:41:05 +0530

    added function to add task in the todo

--- js/app.js
+++ js/app.js
@@ -1,40 +1,31 @@
-let val;
+let form = document.querySelector('#task-form'),
+	  taskList = document.querySelector('.collection'),
+	  clrButton = document.querySelector('.clear-tasks'),
+	  textInput = document.querySelector('#task');
 
-val = document;
-val = document.all;
-val = document.all[2];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
+loadEvent();
+//load all events
+function loadEvent(){
+	form.addEventListener('submit', addTask);
+}
 
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
+//add task event
+function addTask(e){
+	if(textInput.value === ''){
+		alert('Input field is empty.');
+	}
+	
+	let li = document.createElement('li');
+		li.className = 'collection-item';
+		li.appendChild(document.createTextNode(textInput.value));
+	
+	let linkDelete = document.createElement('a');
+		linkDelete.className = 'delete-item secondary-content';
+		linkDelete.innerHTML = '<i class="fa fa-remove"></i>';
+		
+		li.appendChild(linkDelete);
 
-val = document.links;
-val = document.links[0];
-val = document.links[0].id;
-val = document.links[0].className;
-val = document.links[0].classList[0];
-
-val = document.images;
-
-val = document.scripts;
-val = document.scripts[2].getAttribute('src');
-
-let scripts = document.scripts;
-
-let scriptsArr = Array.from(scripts);
-
-scriptsArr.forEach(function(script) {
-	console.log(script.getAttribute('src'));
-});
-
-console.log(val);
+		taskList.appendChild(li);
+		
+	e.preventDefault();
+}
